b824c44e6971ae225b4f1dedef26a24b06de50b4
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-07 11:32:51 -0500

    Initial Commit

---
+++ BeginSwiftUI/BeginSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginSwiftUIApp.swift
+//  BeginSwiftUI
+//
+//  Created by Teodoro Calle Lara on 7/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BeginSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
