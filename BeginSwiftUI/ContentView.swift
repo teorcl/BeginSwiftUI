f29fb48dcf055533a31a9f9ddf53cc37c18dccab
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-08 11:09:10 -0500

    Se crean vistas separadas y se usan los 3 tipos de stacks

--- BeginSwiftUI/ContentView.swift
+++ BeginSwiftUI/ContentView.swift
@@ -9,16 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            PrincipalView()
+            BannerView()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct PrincipalView: View {
+    var body: some View {
+        VStack(
+            alignment: .leading,
+            spacing: 8
+        ) {
+            Text("Siguiendo")
+                .font(.largeTitle)
+                .bold()
+            
+            Text("Canales recomendados")
+                .font(.headline)
+                .foregroundColor(.gray)
+            
+            CardView()
+        }
+        .padding()
+    }
+}
+
+
+struct CardView: View {
+    var body: some View {
+        HStack {
+            // Cuadrado
+            Rectangle()
+                .foregroundStyle(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .frame(
+                    width: 118,
+                    height: 68
+                )
+            VStack (alignment: .leading) {
+                HStack {
+                    //Bola
+                    Circle()
+                        .fill(.blue)
+                        .frame(
+                            width: 24,
+                            height: 24
+                        )
+                    //Username
+                    Text("Username").font(.headline)
+                }
+                Text("Streaming de programación...")
+                    .foregroundStyle(.gray)
+                Text("Solo hablando")
+                    .foregroundStyle(.gray)
+            }
+        }
+    }
+}
+
+struct BannerView: View {
+    var body: some View {
+        Text("Texto Superpuesto")
+            .font(.largeTitle)
+            .bold()
+            .foregroundStyle(.red)
+            .underline()
+            .background(Color.black)
+            .rotationEffect(.degrees(-18))
+    }
 }
